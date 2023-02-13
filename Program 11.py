60fcde70059ad551d488832d9c8d41f797109528
EnriqueM0802 <124806915+EnriqueM0802@users.noreply.github.com>
2023-02-13 14:21:53 -0500

    Delete Program 11.py

--- Program 11.py
+++
@@ -1,8 +0,0 @@
-#Program 11: Containment
-rocks = ["Big Rock", "Little Rock","Tiny Rock", "Giant Rock"]
-quote = "Rocks are always changing and there is the need to always study and watch their transformations"
-
-print("Is Big Rock in the list: ","Big Rock" in rocks)
-print("Is Tiny Rock in the list: ","Tiny Rock" in rocks)
-print("Is study in the quote: ", "study" in quote)
-print("Is IT in the quote: ", "IT" in quote)
